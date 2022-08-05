46f5e6217a53f5e2687ff969607c21095c1ffa94
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-05 18:15:25 +0100

    Adjust database

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS travellers;
 CREATE TABLE travellers (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR ( 20 ) NOT NULL,
+	name VARCHAR ( 50 ) NOT NULL
 );
 COPY travellers
 FROM $$/code/data/travellers.csv$$
@@ -11,7 +11,7 @@ DELIMITER ',' CSV HEADER;
 DROP TABLE IF EXISTS destinations;
 CREATE TABLE destinations (
 	id SERIAL PRIMARY KEY,
-	country VARCHAR ( 20 ),
+	country VARCHAR ( 50 )
 );
 COPY destinations
 FROM $$/code/data/destinations.csv$$
@@ -20,6 +20,14 @@ DELIMITER ',' CSV HEADER;
 DROP TABLE IF EXISTS trips;
 CREATE TABLE trips (
 	id SERIAL PRIMARY KEY,
-	traveller_id INT REFERENCES travellers (id) NOT NULL
+	traveller_id INT REFERENCES travellers (id) NOT NULL,
     destination_id INT REFERENCES destinations (id) NOT NULL 
 );
+COPY trips
+FROM $$/code/data/trips.csv$$
+DELIMITER ',' CSV HEADER;
+
+
+SELECT travellers.name AS traveller, destinations.country AS destination
+FROM ((trips JOIN travellers ON trips.traveller_id = travellers.id)
+JOIN destinations ON trips.destination_id = destinations.id);
